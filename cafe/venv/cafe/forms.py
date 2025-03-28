2ac88147ce9043809008466dd4e90ef9d86aead1
BodeMurairi2 <b.murairi@alustudent.com>
2025-03-28 04:06:42 +0200

    Cafe finale

--- cafe/venv/cafe/forms.py
+++ cafe/venv/cafe/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditorField
-
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 class AddCafeForm(FlaskForm):
     name = StringField('Coffee Name', validators=[DataRequired()])
     Location = StringField('Location', validators=[DataRequired()])
     Availability = StringField('Availability', validators=[DataRequired()])
+    submit = SubmitField('➕ Add Cafe')
--- cafe/venv/cafe/main.py
+++ cafe/venv/cafe/main.py
@@ -2,50 +2,46 @@
 import secrets
 from flask import Flask, request, jsonify, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
 from forms import AddCafeForm
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex(16)
-Bootstrap(app)
+Bootstrap5(app)
+csrf = CSRFProtect(app)
 
 class Base(DeclarativeBase):
     pass
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coffee.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 # Coffee Table
-
 class CoffeeTable(db.Model):
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     CoffeeName: Mapped[str] = mapped_column(String(500), unique=True)
     Location: Mapped[str] = mapped_column(String(500))
     Availability_details: Mapped[str] = mapped_column(String(2000))
 
-
 with app.app_context():
     db.create_all()
-
-
 
 # Running the app
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/cafe/all-cafe')
 def view_cafe():
     all_cafes = CoffeeTable.query.all()
+    for i in all_cafes:
+        print(i)
     return render_template('cafes.html', cafes=all_cafes)
-
 
 @app.route("/cafe/add-cafe", methods=["GET", "POST"])
 def add_cafe():
@@ -63,8 +59,7 @@ def add_cafe():
         db.session.commit()
         return redirect(url_for('view_cafe'))
 
-    return render_template('add-cafe.html', form=add_form)
-
+    return render_template('add-cafe.html', form=add_form) #Ensured form is passed.
 
 if __name__ == "__main__":
     app.run(debug=True)
