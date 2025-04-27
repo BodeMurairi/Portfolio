6d634e025f0fd83318e0c993f706eaad62330796
BodeMurairi2 <b.murairi@alustudent.com>
2025-04-27 14:41:11 +0200

    Webscraping

--- webscraping/main.py
+++ webscraping/main.py
@@ -6,7 +6,12 @@ This script collects data about top entrepreneurial leadership books
 from theceolibrary and saves it to a pdf file
 this file is sent to the user via email
 '''
+
+# Keep Chrome opened
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('detach', True)
+
 # Create a new driver instance
-driver = webdriver.chrome()
+driver = webdriver.Chrome(options=chrome_options)
 # Open the URL
 driver.get("https://theceolibrary.com/")
